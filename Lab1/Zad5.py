b961721b165abc7980f9001b3b0d0aa47497de09
MarciniecMarcin <150946357+MarciniecMarcin@users.noreply.github.com>
2023-11-16 13:05:43 +0100

    rozwiązanie zadań Lab1

--- Lab1/Zad5.py
+++ Lab1/Zad5.py
@@ -2,9 +2,21 @@ print("program do porządkowania 3 liczb od najmniejszej do największej")
 x = int(input("wprowadź pierwszą liczbę: "))
 y = int(input("wprowadź drugą liczbę: "))
 z = int(input("wprowadź trzecia liczbę: "))
+lista1 = (x, y, z) #zmienna pobierająca dane od użytkownika
+lista  = list(lista1) #zmiana na listę
+def sortowanie(lista):
+    n = len(lista)
 
-if x > y and x > z and y > z:
-    print(z, y, x)
-elif x < y and x > z and y > z:
-    print(z, x, y)
-elif
+    while n > 1:
+        zamien = False
+        for l in range(0, n - 1):
+            if lista[l] > lista[l + 1]:
+                lista[l], lista[l + 1] = lista[l + 1], lista[l]
+                zamien = True
+
+        n -= 1
+        print(lista)
+        if zamien == False: break
+
+    return lista
+sortowanie(lista)
